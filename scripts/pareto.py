f8c463fb535d35aca143cb1e39f8a90126ea8c39
Muhammad Yudistira Hanifmuti <muhammad.yudistira@ui.ac.id>
2019-10-21 12:32:19 +0700

    add audio routine

--- scripts/pareto.py
+++ scripts/pareto.py
@@ -1,6 +1,7 @@
 import imutils
 import cv2
 import numpy as np
+import pyaudio
 
 from picamera import PiCamera
 from time import sleep
@@ -56,12 +57,37 @@ def img_routine():
         img = cv2.imread('../img/pic-'+str(i)+'.jpg')
         res, val_max, res_img = template_match(template, img)
         print(res, val_max)
-        cv2.imwrite('../img/pic-'+'recognized-'+i+'.png', res_img)
+        cv2.imwrite('../img/pic-'+'recognized-'+str(i)+'.png', res_img)
         summary += int(res)
     return summary
 
+def audio_routine():
+    #The following code comes from markjay4k as referenced below
+
+    chunk=4096
+    RATE=44100
+
+    p=pyaudio.PyAudio()
+
+    #input stream setup
+    stream=p.open(format = pyaudio.paInt16,rate=RATE,channels=1, input_device_index = 2, input=True, frames_per_buffer=chunk)
+
+    #the code below is from the pyAudio library documentation referenced below
+    #output stream setup
+    player=p.open(format = pyaudio.paInt16,rate=RATE,channels=1, output=True, frames_per_buffer=chunk)
+
+    while True:            #Used to continuously stream audio
+        data=np.fromstring(stream.read(chunk,exception_on_overflow = False),dtype=np.int16)
+        print(data)
+        player.write(data,chunk)
+        
+    #closes streams
+    stream.stop_stream()
+    stream.close()
+    p.terminate
 
 camera = PiCamera()
 camera.rotation = 270
-take_pic(camera)
-img_routine()
+# take_pic(camera)
+# img_routine()
+audio_routine()
